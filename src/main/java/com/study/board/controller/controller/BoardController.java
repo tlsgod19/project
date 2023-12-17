73b904e031a53aa343e2600c59cee49a05c486fa
sinhang <tlsgod39@naver.com>
2023-12-17 22:32:23 +0900

    test

--- src/main/java/com/study/board/controller/controller/BoardController.java
+++ src/main/java/com/study/board/controller/controller/BoardController.java
@@ -4,6 +4,7 @@ import com.study.board.entity.Board;
 import com.study.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,12 +19,18 @@ public class BoardController {
     private BoardService boardService;
     @GetMapping("/board/write")//localhost://8080/board/write
     public  String boardWriteForm(){
-
+        System.out.println("22");
         return "boardwrite";
     }
     @PostMapping("/board/writepro")
     public String boardWritePro(Board board){
         boardService.write(board);
         return "";
+    }
+
+    @GetMapping("/board/list")
+    public  String boardlist(Model model){
+        model.addAttribute("list",boardService.boardList());
+        return "boardlist";
     }
 }
